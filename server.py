87c1d90de8dcf8376d02ea1d2bd69abd4e9d2248
Philipp Münch <philipp.muench@helmholtz-hzi.de>
2020-01-15 23:32:43 +0100

    initial commit

---
+++ server.py
@@ -0,0 +1,23 @@
+import h5py
+import json
+import pandas as pd
+import numpy as np
+
+from flask import Flask, render_template
+
+filename = 'data.h5'
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # load data
+    with h5py.File(filename, 'r') as f:
+        states = f['states']
+        data = np.array(states.value)
+        data = np.amax(data, axis=0)[1:1500]
+
+    return render_template("index.html", data=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
